c4201d9f023a9ca12a2416204fc2d3cbb13ffc6d
Dustin <dustincheyney@gmail.com>
2024-10-04 16:19:09 -0700

    final model work

--- backend/models/Firms.js
+++ backend/models/Firms.js
@@ -2,6 +2,28 @@ const db = require("../db");
 const { BadRequestError, NotFoundError } = require("../expressError");
 const { sqlForPartialUpdate } = require("../helpers/updateSql");
 
-class Firm {}
+class Firm {
+  // Get a list of firms by state and county
+  static async getFirms({ data }) {
+    const res = await db.query(
+      `SELECT * FROM firms WHERE state_id = $1 AND county_id = $2`,
+      [data.stateId, data.countyId]
+    );
+    const firms = res.rows;
+    return firms;
+  }
+
+  // Get a certain firm by id
+  static async getFirm({ id }) {}
+
+  // Add a firm
+  static async addFirm({ data }) {}
+
+  // Remove a firm
+  static async removeFirm({ id }) {}
+
+  //Update a firm
+  static async updateFirm({ data }) {}
+}
 
 module.exports = Firm;
