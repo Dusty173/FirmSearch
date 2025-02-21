e3aecec5e7c493d236f47a5ed1be3c27c949bbc3
dustinc <dustincheyney@gmail.com>
2025-02-21 10:50:55 -0800

    Adding frontend files

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,18 +1,5 @@
 import React from "react";
 import { Routes, Route, useNavigate, Outlet } from "react-router-dom";
-import Homepage from "./hompage/Home";
-import PostsPage from "./posts/PostsPage";
-import LoginForm from "./forms/LoginForm";
-import SignupForm from "./forms/Signupform";
-import ProfileForm from "./forms/ProfileForm";
-import PostForm from "./posts/AddPost";
-import DrivesList from "./drives/DriveList";
-import DriveForm from "./drives/AddDriveForm";
-import UserProfile from "./Profile";
-import MyGarage from "./cars/mygarage";
-import CarForm from "./forms/newCar";
-import PostDetail from "./posts/PostDetail";
-import DriveDetail from "./drives/DriveDetails";
 
 function Routing({ login, signup }) {
   const Navigate = useNavigate();
@@ -38,18 +25,7 @@ function Routing({ login, signup }) {
         <Route path="/signup" element={<SignupForm signup={signup} />}></Route>
 
         <Route element={<PrivateRoutes />}>
-          <Route path="/posts" element={<PostsPage />} />
-          <Route path="/posts/:id" element={<PostDetail />} />
-          <Route path="/posts/new" element={<PostForm />} />
-          <Route path="/users" element={<UserProfile />} />
-          <Route path="/users/edit" element={<ProfileForm />} />
-          <Route path="/drives" element={<DrivesList />} />
-          <Route path="/drives/new" element={<DriveForm />} />
-          <Route path="/drives/:title" element={<DriveDetail />} />
-          <Route path="/cars" element={<MyGarage />} />
-          <Route path="/cars/new" element={<CarForm />} />
-          <Route path="/drives/:title/join" element={<DriveDetail />} />
-          <Route path="/drives/:title/leave" element={<DriveDetail />} />
+          {/* Add Admin routes here */}
         </Route>
       </Routes>
     </>
